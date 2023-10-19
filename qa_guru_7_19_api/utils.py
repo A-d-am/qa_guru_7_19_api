5c1a648e4c7dab046f84078574a3a1e78442525b
A-d-a-m <adam3928@mail.ru>
2023-10-19 15:40:31 +0200

    added JSON_DIR to def load_schema

--- qa_guru_7_19_api/utils.py
+++ qa_guru_7_19_api/utils.py
@@ -1,9 +1,13 @@
 import json
 import os
 
+CURRENT_FILE_PATH = os.path.abspath(__file__)
+ROOT_PATH = os.path.dirname(CURRENT_FILE_PATH)
+JSON_DIR = os.path.join(ROOT_PATH, 'json_schemas')
 
-def load_schema(method_schema):
-    path = os.path.join('json_schemas', method_schema)
+
+def load_schema(api_name, method_schema):
+    path = os.path.join(JSON_DIR, api_name, method_schema)
     with open(path) as file:
         json_schema = json.loads(file.read())
     return json_schema
